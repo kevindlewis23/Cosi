801a3605484cf5e7cdb7a637b2d2ef5b938c6cc8
Kevin Lewis <55998034+kevindlewis23@users.noreply.github.com>
2024-10-30 21:47:09 +0000

    Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunc() {
+  document.getElementById("myParagraph").textContent = "New Text"
+  document.getElementById("myParagraph").style.color = "red"
+  document.getElementById("myParagraph").style.fontSize = "2em"
+}
